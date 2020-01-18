471e67698ab46b27ecd5931d8667ad59458a907d
sungchuni <new.sungchuni@gmail.com>
2020-01-18 23:09:41 +0900

    fix: update geo func

--- src/Sphere/geo.js
+++ src/Sphere/geo.js
@@ -7,15 +7,16 @@ export default function geo() {
     .map(function getTheta(noop, index) {
       return Math.PI * (index / (thetasLength - 1)) - Math.PI * 0.5;
     })
-    .map(function getPhis(theta) {
+    .map(function getPhis(theta, thetaIndex) {
       const maxPhis = (thetasLength - 1) * (Math.PI * 0.5);
       const phisLength = Math.round(
-        Math.cos(theta) * (maxPhis * (Math.PI / 2)) + maxPhis
+        Math.cos(theta) * (maxPhis * (Math.PI / 2)) +
+          maxPhis * (Math.abs(thetaIndex / (thetasLength - 1) - 0.5) * 2)
       );
       const phis = Array(phisLength)
         .fill(null)
-        .map(function getPhi(noop, index) {
-          return Math.PI * 2 * (index / (phisLength - 1));
+        .map(function getPhi(noop, phiIndex) {
+          return Math.PI * 2 * (phiIndex / (phisLength - 1));
         });
       return { theta, phis };
     });

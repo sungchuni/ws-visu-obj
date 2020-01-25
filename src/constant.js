db7e0b40dbcc87a7ca31795f253617ea8c79dddd
sungchuni <new.sungchuni@gmail.com>
2020-01-26 00:56:52 +0900

    fix: imporove 8abfd3e commit

--- src/constant.js
+++ src/constant.js
@@ -3,6 +3,6 @@ export const RESIZE_WAIT = 400;
 export const COLOR = "#535353";
 export const COLOR_MINOR = "#d3d3d3";
 export const COLOR_SHADOW = "#999999";
-export const SHADOW_OFFSET = 16;
+export const SHADOW_OFFSET = 0;
 
 export const TRAIT_DRAW_SHADOW_BEZIER_OFFSET = 4;
--- src/trait.js
+++ src/trait.js
@@ -5,12 +5,16 @@ export function drawShadow(ctx, options, clientRect) {
   const { width, height } = clientRect;
   const start = width * 0.5 - Math.min(width * 0.8, shadowSize) * 0.5;
   const end = start + Math.min(width * 0.8, shadowSize);
-  const y = height + shadowOffset;
+  const y = height + TRAIT_DRAW_SHADOW_BEZIER_OFFSET + shadowOffset;
   const bezierOffset = TRAIT_DRAW_SHADOW_BEZIER_OFFSET;
   ctx.fillStyle = shadowColor;
   ctx.shadowColor = shadowColor;
   ctx.shadowBlur = shadowBlur;
-  ctx.shadowOffsetY = -shadowOffset * 3;
+  ctx.shadowOffsetY = -(
+    TRAIT_DRAW_SHADOW_BEZIER_OFFSET * 2 +
+    shadowBlur * 2 +
+    shadowOffset
+  );
   ctx.beginPath();
   ctx.moveTo(start, y);
   ctx.bezierCurveTo(start, y - bezierOffset, end, y - bezierOffset, end, y);
